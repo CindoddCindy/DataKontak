5c322a21b2533e8920c15f1830cd69db77aab3db
CindoddCindy <cindodcindy@gmail.com>
2020-10-17 20:58:49 +0700

    tambah layout

---
+++ app/src/main/java/cindodcindy/tonjootest/contactlist/view/RvContactList.java
@@ -0,0 +1,14 @@
+package cindodcindy.tonjootest.contactlist.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RvContactList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rv_contact_list);
+    }
+}
