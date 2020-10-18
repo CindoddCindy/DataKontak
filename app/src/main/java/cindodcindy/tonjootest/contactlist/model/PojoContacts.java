5e4a7eaeacaa1401372e6d597e5902d7ffc61d7b
CindoddCindy <cindodcindy@gmail.com>
2020-10-18 11:41:51 +0700

    tambah sqlite

---
+++ app/src/main/java/cindodcindy/tonjootest/contactlist/model/PojoContacts.java
@@ -0,0 +1,71 @@
+package cindodcindy.tonjootest.contactlist.model;
+
+public class PojoContacts {
+    private int idContact;
+    private String fisrtNameContact;
+    private String lastNameContact;
+    private String emailUserContact;
+    private String genderUserContact;
+    private String fotoUserContact;
+
+    public PojoContacts(){
+
+    }
+
+    public PojoContacts(int idContact, String fisrtNameContact, String lastNameContact, String emailUserContact, String genderUserContact, String fotoUserContact) {
+        this.idContact = idContact;
+        this.fisrtNameContact = fisrtNameContact;
+        this.lastNameContact = lastNameContact;
+        this.emailUserContact = emailUserContact;
+        this.genderUserContact = genderUserContact;
+        this.fotoUserContact = fotoUserContact;
+    }
+
+    public int getIdContact() {
+        return idContact;
+    }
+
+    public void setIdContact(int idContact) {
+        this.idContact = idContact;
+    }
+
+    public String getFisrtNameContact() {
+        return fisrtNameContact;
+    }
+
+    public void setFisrtNameContact(String fisrtNameContact) {
+        this.fisrtNameContact = fisrtNameContact;
+    }
+
+    public String getLastNameContact() {
+        return lastNameContact;
+    }
+
+    public void setLastNameContact(String lastNameContact) {
+        this.lastNameContact = lastNameContact;
+    }
+
+    public String getEmailUserContact() {
+        return emailUserContact;
+    }
+
+    public void setEmailUserContact(String emailUserContact) {
+        this.emailUserContact = emailUserContact;
+    }
+
+    public String getGenderUserContact() {
+        return genderUserContact;
+    }
+
+    public void setGenderUserContact(String genderUserContact) {
+        this.genderUserContact = genderUserContact;
+    }
+
+    public String getFotoUserContact() {
+        return fotoUserContact;
+    }
+
+    public void setFotoUserContact(String fotoUserContact) {
+        this.fotoUserContact = fotoUserContact;
+    }
+}
--- app/src/main/java/cindodcindy/tonjootest/contactlist/sqlite/AddContactData.java
+++ app/src/main/java/cindodcindy/tonjootest/contactlist/sqlite/AddContactData.java
@@ -1,6 +1,7 @@
 package cindodcindy.tonjootest.contactlist.sqlite;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,16 +9,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cindodcindy.tonjootest.contactlist.model.PojoContacts;
+
 public class AddContactData extends SQLiteOpenHelper {
-    private static final int VERSIONANSW = 3;
-    private static final String DBNAMEANSW = "db_answer";
+    private static final int VERSIONANSW = 1;
+    private static final String DBCONTACT = "db_answer";
     private static final String TABLENAME = "answer";
 
-    private static String colIDAnsw = "id_answ";
-    private static String colTanggalPr = "tanggal_pr";
-    private static String colDoaPr = "doa_pr";
-    private static String colTanggalAnsw = "tanggal_answ";
-    private static String colDoaPrAnsw = "doa_answ";
+    private static String colIdContacts="idCont";
+    private static String colFirstName = "firstName";
+    private static String colLastName = "lastName";
+    private static String colEmail = "emailUser";
+    private static String colGender = "genderUser";
+    private static String colPhoto = "photoUser";
+
+    public AddContactData(Context context) {
+        super(context, DBCONTACT, null, VERSIONANSW);
+    }
+
 
     public AddContactData(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -30,11 +42,83 @@ public class AddContactData extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTable = "CREATE TABLE " + TABLENAME + " (" +
+                colIdContacts + " INTEGER PRIMARY KEY AUTOINCREMENT, " + colFirstName + " TEXT," + colLastName + " TEXT, " + colEmail + " TEXT, " + colGender + "TEXT" +
+                colPhoto + " TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ TABLENAME);
+        onCreate(sqLiteDatabase);
+
 
     }
+    public void insertDataAnsw(String firstName, String lastName, String emailUser, String genderUser, String photoUser){
+        String insertData = "INSERT INTO "+ TABLENAME + " ("+ colFirstName +","+colLastName+","+colEmail+","+colGender+","+colPhoto+") VALUES ('"+firstName +"', '"+lastName+"','"+emailUser+"','"+genderUser+"','"+photoUser+"')";
+        this.getWritableDatabase().execSQL(insertData);
+    }
+
+    public void updateDataAnsw(int id,String firstName, String lastName, String emailUser, String genderUser, String photoUser ){
+        String updateData = "UPDATE "+TABLENAME+ " SET "+ colFirstName + "= '"+firstName +"', "+colLastName + "= '"+lastName + "',"+colEmail+ "='"+emailUser+"',"+colGender+"='"+genderUser+"',"+colPhoto+"='"+photoUser+"' WHERE "+colIdContacts+" ="+id;
+        this.getWritableDatabase().execSQL(updateData);
+    }
+
+    public void deleteDataAnsw(int id){
+        String deleteData = "DELETE FROM "+TABLENAME +" WHERE idCont="+id;
+        this.getWritableDatabase().execSQL(deleteData);
+    }
+
+    public PojoContacts getPojoContacts(int id){
+        PojoContacts pojoContacts = null;
+        String selectData = "SELECT * FROM "+TABLENAME + " WHERE idCont="+String.valueOf(id);
+        Cursor data = this.getWritableDatabase().rawQuery(selectData, null);
+        if(data.moveToFirst()){
+            pojoContacts = new PojoContacts(Integer.parseInt(data.getString(data.getColumnIndex(colIdContacts))),
+                    data.getString(data.getColumnIndex(colFirstName)), data.getString(data.getColumnIndex(colLastName)),data.getString(data.getColumnIndex(colEmail)),data.getString(data.getColumnIndex(colGender)),data.getString(data.getColumnIndex(colPhoto)));
+        }
+        return pojoContacts;
+    }
+
+    public List<PojoContacts> getAllConatacts(){
+        List<PojoContacts> pojoContacts = new ArrayList<>();
+        String selectData = "SELECT * FROM "+TABLENAME;
+        Cursor data = this.getWritableDatabase().rawQuery(selectData, null);
+        if(data.moveToFirst()){
+            do{
+                pojoContacts.add(new PojoContacts(Integer.parseInt(data.getString(data.getColumnIndex(colIdContacts))),
+                        data.getString(data.getColumnIndex(colFirstName)), data.getString(data.getColumnIndex(colLastName)),data.getString(data.getColumnIndex(colEmail)),data.getString(data.getColumnIndex(colGender)),data.getString(data.getColumnIndex(colPhoto))));
+            }while (data.moveToNext());
+        }
+        return pojoContacts;
+    }
+
+    public List<PojoContacts> searchAns(String keyword) {
+        List<PojoContacts> pojoAnsws = null;
+        try {
+            SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+            Cursor cursor = sqLiteDatabase.rawQuery("select * from " + TABLENAME + " where " + colFirstName + " like ?", new String[] { "%" + keyword + "%" });
+            if (cursor.moveToFirst()) {
+                pojoAnsws = new ArrayList<PojoContacts>();
+                do {
+                    PojoContacts pojoContacts= new PojoContacts();
+                    pojoContacts.setIdContact(cursor.getInt(0));
+                    pojoContacts.setFisrtNameContact(cursor.getString(1));
+                    pojoContacts.setLastNameContact(cursor.getString(2));
+                    pojoContacts.setEmailUserContact(cursor.getString(3));
+                    pojoContacts.setGenderUserContact(cursor.getString(4));
+                    pojoAnsws.add(pojoContacts);
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            pojoAnsws = null;
+        }
+        return pojoAnsws;
+    }
+
+
+
 }
